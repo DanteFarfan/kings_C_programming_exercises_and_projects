dc503342e4250656c23368f2e198bc118906cc16
DanteFarfan <129178199+DanteFarfan@users.noreply.github.com>
2026-01-30 20:22:43 -0300

    Solution to exercise 6 from chapter 3

---
+++ Exercises/Chapter3/Exercise6/exercise6.c
@@ -0,0 +1,20 @@
+//Author: Dante Farfán
+//Description: Adds two fractions
+
+#include<stdio.h>
+
+int main(void)
+{
+    int num1, denom1, num2, denom2, result_num, result_denom;
+
+    printf("Enter first fraction: ");
+    scanf("%d / %d", &num1, &denom1);
+
+    printf("Enter second fraction: ");
+    scanf("%d / %d", &num2, &denom2);
+
+    result_num = num1 * denom1 + num2 * denom2;
+    result_denom = denom1 * denom2;
+
+    printf("The result is: %d/%d\n", result_num, result_denom);
+}
