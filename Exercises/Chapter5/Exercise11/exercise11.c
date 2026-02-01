535416a99f9b4c6e67e54fa1248d8cd80b6b5784
DanteFarfan <129178199+DanteFarfan@users.noreply.github.com>
2026-02-01 03:26:56 -0300

    Solution to exercise 11 from chapter 5

---
+++ Exercises/Chapter5/Exercise11/exercise11.c
@@ -0,0 +1,34 @@
+//Author: Dante Farfán
+//Description: Matches area code to city name
+
+#include <stdio.h>
+
+int main(void)
+{
+    int area_code = 0;
+
+    printf("Enter an area code: ");
+    scanf("%d", &area_code);
+
+    switch (area_code)
+    {
+        case 229:
+            printf("Albany");
+            break;
+        case 404: case 470: case 678: case 770:
+            printf("Atlanta");
+            break;
+        case 478:
+            printf("Macon");
+            break;
+        case 706: case 762:
+            printf("Columbus");
+            break;
+        case 912:
+            printf("Savannah");
+            break;
+        default:
+            printf("Area code not found!");
+            break;
+    }
+}
